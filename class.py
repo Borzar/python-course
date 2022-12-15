82aed5778479f18f21944a498824226a4d134e9f
Boris Zarate <boris.zlobos@gmail.com>
2022-12-15 19:28:36 -0300

    re commit

---
+++ class.py
@@ -0,0 +1,83 @@
+class Casa:
+    def __init__(self, color, cantidad_pisos):
+        self.color = color
+        self.cantidad_pisos = cantidad_pisos
+
+casa_blanca = Casa("blanco", 4)
+
+
+class Cubo:
+    def __init__(self, color):
+        self.color = color
+
+    caras = 6
+
+cubo_rojo = Cubo('rojo')
+
+class Personaje:
+    real = False
+    def __init__(self, especie, magico, edad):
+        self.especie = especie
+        self.magico = magico
+        self.edad = edad
+
+harry_potter = Personaje("Humano", True, 17)
+
+class Perro:
+    def ladrar(self):
+        print("Guau!")
+
+dalton = Perro()
+dalton.ladrar()
+
+class Mago:
+    def lanzar_hechizo(self):
+        print("¡Abracadabra!")
+
+merlin = Mago()
+merlin.lanzar_hechizo()
+
+class Alarma:
+    def postergar(self, cantidad_minutos):
+        print(f"La alarma ha sido pospuesta {cantidad_minutos} minutos")
+
+mialarma = Alarma()
+mialarma.postergar(30)
+
+
+class Mascota: 
+    @staticmethod
+    def respirar():
+        print("Inhalar... Exhalar")
+
+Mascota.respirar()
+
+class Jugador:
+
+    vivo = False
+
+    @classmethod
+    def revivir(cls):
+        cls.vivo = True 
+
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+class Alumno(Persona):
+    pass
+
+boris = Alumno('Boris', 31)
+
+
+class Pajaro:
+    def __init__(self, edad, nombre, cantidad_patas):
+        self.edad = edad
+        self.nombre = nombre 
+        self.cantidad_patas = cantidad_patas 
+
+class Paloma(Pajaro):
+    pass
+
+tututu = Paloma(10, 'Dalton', 4)
---
+++ files.py
@@ -0,0 +1,15 @@
+### create and write on files
+# my_file = open('Prueba.txt', 'w')
+# my_file.write("Nuevo Texto")
+# my_file.close()
+# my_file = open('Prueba.txt', 'r')
+# print(my_file.read())
+
+# my_file = open('mi_archivo.txt', 'a')
+# my_file.write('Nuevo inicio de sesión')
+# my_file.close()
+
+# my_file = open('mi_archivo.txt')
+# print(my_file.read())
+
+
